7d3fa49e70633c1c3ff5e1478d90aa59dec3cd38
권은서 <kwonenseo@gmail.com>
2022-07-07 14:03:19 +0900

    feat: header component

---
+++ src/components/shared/header/header.style.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const HeaderWrapper = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 80px;
+  padding: 10px 42px;
+  border-bottom: 1px solid var(--color-light-gray);
+`
+export const H1 = styled.h1`
+  font-size: 20px;
+  font-weight: 700;
+`
---
+++ src/components/shared/header/index.tsx
@@ -0,0 +1,9 @@
+import * as Styled from './header.style'
+
+export default function Header() {
+  return (
+    <Styled.HeaderWrapper>
+      <Styled.H1>TRIPLE</Styled.H1>
+    </Styled.HeaderWrapper>
+  )
+}
